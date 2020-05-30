449b3c1285dd50811f3d89a9a680c554ab0dd61e
Adel Muursepp <adel.muursepp@gmail.com>
2020-05-30 09:55:23 +0300

    done 3 first pages

--- src/components/SolutionsIntro.js
+++ src/components/SolutionsIntro.js
@@ -10,7 +10,7 @@ class SolutionsIntro extends React.Component {
                 <div className="col-lg-12">
                     <div className="features-box">
                         <h3>Solutions</h3>
-                        <p className=" web-desc">Our solution is a software deployment model in an enterprise application which is delivered and managed as a service to customers simultaneously. Our software as a service model enables every partner to benefit from the latest technological features without the disruption and costs associated with software updates and upgrades. Our solution services the needs of a partner network</p>
+                        <p className=" web-desc">If you’re a consumer our solution will save your family countless hours of administrative headaches by sending notifications you pre-select; so they don’t have to.  See our IvyLocker® Product.</p>
                         <p className=" web-desc">Benefits of IvyNet solution which may be realized by any partner</p>
                 
 

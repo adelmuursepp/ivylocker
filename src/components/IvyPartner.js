728888ea8fba675f5f58562a72639e973ab2a599
Adel Muursepp <adel.muursepp@gmail.com>
2020-06-04 22:51:36 +0300

    link and href

--- src/components/IvyPartner.js
+++ src/components/IvyPartner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class IvyPartner extends React.Component {
   render() {
--- src/components/NavbarNoTransition.js
+++ src/components/NavbarNoTransition.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 class NavbarNoTransition extends React.Component {
   render() {
